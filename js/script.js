925a9645ea6382f8ba00309cd68d7520680c2f00
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-05-09 20:38:56 +0530

    animation added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function() {
+	header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 let menu = document.querySelector('#menu-icon');
@@ -17,6 +17,7 @@ window.onscroll = () => {
 	navlist.classList.remove('active');
 };
 
+/*
 const sr = ScrollReveal ({
 	distance: '45px',
 	duration: 2700,
@@ -27,13 +28,10 @@ sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
-
+*/
 
 // let form  = document.getElementById("form").addEventListener("submit",formFnc);
 
-// function  formFnc(){
+// function formFnc() {
 // 	event.preventDefault();
-
-	
-
 // }
